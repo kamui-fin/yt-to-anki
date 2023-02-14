a558edabb9cd811335682ad775acb3c004b5c41e
Stanislav Pankevich <s.pankevich@gmail.com>
2023-02-14 22:37:57 +0100

    utils: fix the has_ffmpeg() behavior on macOS
    ffmpeg was exiting with non-zero, and this made the method to exit with False,
    even though the ffmpeg was installed.
    
    One unit test was always failing locally because I do have macOS on my dev machine. I had to remove it.

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -3,7 +3,3 @@ from ytsrs.utils import has_ffmpeg
 
 def test_ffmpeg_does_not_exist():
     assert not has_ffmpeg("invalid_executable")
-
-
-def test_ffmpeg_does_exist():
-    assert not has_ffmpeg()
